6153527771a9e3ebba592e84fd550c908458d4a8
Dalia Azzoug <dalia.azzoug2018@gmail.com>
2025-10-28 20:57:53 +0100

    Creation du fond

---
+++ Snake game.py
@@ -0,0 +1,14 @@
+import turtle
+
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("pink")
+wn.setup(width=600, height=600)
+wn.tracer(0)  # Turns off the screen updates
+
+
+
+wn.mainloop() 
+ 
