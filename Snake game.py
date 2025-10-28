8c949b59ce2d3bebe36dfa40145c0a26fd5362d7
Dalia Azzoug <dalia.azzoug2018@gmail.com>
2025-10-28 21:50:03 +0100

    Mouvements snake

--- Snake game.py
+++ Snake game.py
@@ -1,4 +1,9 @@
 import turtle
+import time
+
+
+
+delay = 0.1
 
 
 # Set up the screen
@@ -8,6 +13,14 @@ wn.bgcolor("pink")
 wn.setup(width=600, height=600)
 wn.tracer(0)  # Turns off the screen updates
 
+
+#quitter
+running = True
+def stop_game():
+    global running
+    print("Game Stopped")
+    running = False
+    
 #Snake head 
 head = turtle.Turtle()
 head.speed(0)
@@ -17,11 +30,56 @@ head.penup()
 head.goto(0,0)
 head.direction = "stop" 
 
-# Main game loop
-while True:
-    wn.update()         
+# Functions to control the snake
+def go_up():
     
+        head.direction = "up"
+def go_down():
+        head.direction = "down"
+def go_left():
+        head.direction = "left"
+def go_right():
+        head.direction = "right"
 
 
-wn.mainloop() 
- 
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)  
+
+        
+# Keyboard bindings
+wn.listen()
+
+wn.onkeypress(go_up, "z")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "q")
+wn.onkeypress(go_right, "d")
+
+wn.onkey(stop_game, "Escape")
+
+try:
+    while running:
+        wn.update()
+        move()
+        time.sleep(delay)
+except turtle.Terminator:
+    # Fenêtre fermée pendant la boucle -> on sort sans crasher
+    pass
+finally:
+    try:
+        wn.bye()
+    except turtle.Terminator:
+        pass
