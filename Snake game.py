91eed17f4e062aa46ead5bca2286f8486ee55ece
Dalia Azzoug <dalia.azzoug2018@gmail.com>
2025-10-28 21:04:27 +0100

    Snake head

--- Snake game.py
+++ Snake game.py
@@ -8,6 +8,19 @@ wn.bgcolor("pink")
 wn.setup(width=600, height=600)
 wn.tracer(0)  # Turns off the screen updates
 
+#Snake head 
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black") 
+head.penup()
+head.goto(0,0)
+head.direction = "stop" 
+
+# Main game loop
+while True:
+    wn.update()         
+    
 
 
 wn.mainloop() 
